8838a35e815a1bfe7689a70713ba82695f238b58
Antony Torres <antonyetr@outlook.com>
2023-10-05 14:00:37 -0600

    Implemente codigo para validacion en js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let signUpForm = document.getElementById("sign-up-form")
+signUpForm.noValidate = true;
 
+signUpForm.addEventListener("submit", validateForm)
+function validateForm(e) {
+  const form = e.target;
+  if(form.checkValidity()){
+    e.preventDefault()
+    console.log("Valid!");
+  }else{
+    e.preventDefault()
+    console.log("Invalid!");
+    Array.from(signUpForm.elements).forEach(i => {
+      if(i.checkValidity()){
+        i.classList.remove("invalid")
+      }else{
+        i.classList.add("invalid")
+      }
+    })
+  }
+}
