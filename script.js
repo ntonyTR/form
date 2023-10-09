a02d013ec37cab9496154652b819a5fde96d1149
Antony Torres <antonyetr@outlook.com>
2023-10-08 19:30:42 -0600

    implemente los validaciones en el form, con estilos

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
-let signUpForm = document.getElementById("sign-up-form")
+  let signUpForm = document.getElementById("sign-up-form");
 signUpForm.noValidate = true;
 
-signUpForm.addEventListener("submit", validateForm)
+signUpForm.addEventListener("submit", validateForm);
+
 function validateForm(e) {
   const form = e.target;
-  if(form.checkValidity()){
-    e.preventDefault()
-    console.log("Valid!");
+  if (form.checkValidity()) {
+    e.preventDefault(); // TEST
+    console.log("Valid!"); // TEST
+  } else {
+    e.preventDefault(); // TEST
+    console.log("Invalid!"); // TEST
+
+    highlightInvalid(signUpForm)
+  }
+}
+
+function highlightInvalid(form){
+  Array.from(form.elements).forEach((ele) => {
+    if(ele.checkValidity()){
+      ele.classList.remove("invalid");
+    } else {
+      ele.classList.add("invalid");
+    }
+  })
+}
+
+signUpForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  let password = document.getElementById("password").value;
+  let passwordConfirm = document.getElementById("password-confirm").value;
+
+  console.log("password:", password);
+  console.log("confirm password:", passwordConfirm);
+
+  if(password === passwordConfirm){
+    console.log("Match!");
   }else{
+    console.log("Mismatch!");
+  }
+});
+
+function validatePasswords(e){
+  let password = document.getElementById("password").value;
+  let passwordConfirm = document.getElementById("password-confirm").value;
+
+  if(password === passwordConfirm){
+    console.log("MATCH"); //TEST
+  }else{
+    console.log("MISMATCH"); //TEST
     e.preventDefault()
-    console.log("Invalid!");
-    Array.from(signUpForm.elements).forEach(i => {
-      if(i.checkValidity()){
-        i.classList.remove("invalid")
-      }else{
-        i.classList.add("invalid")
-      }
-    })
+    //AQUI
   }
 }
